fca12bfda3a74f953a4aabeccd9f5d37405a2ed3
huc003 <459382234@qq.com>
2017-01-24 16:36:58 +0800

    项目提交

--- src/main/java/com/huc/action/HelloController.java
+++ src/main/java/com/huc/action/HelloController.java
@@ -1,13 +1,13 @@
 package com.huc.action;
 
-import com.huc.service.HelloService;
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.annotation.Resource;
+import com.huc.service.HelloService;
 
 @Controller
 @RequestMapping("/")
